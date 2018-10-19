46f2303f4cc4ee1553a70954610f0541668ff66c
brkcllakyz21 <37548000+brkcllakyz21@users.noreply.github.com>
2018-10-20 00:35:38 +0300

    Add files via upload

---
+++ Hypotenuse/Hypotenuse/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hypotenuse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double kenar_1 = 0, kenar_2 = 0, Hipotenüs = 0;
+            Console.Write("1. Dik Kenarı Giriniz = ");
+            kenar_1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("2. Dik Kenarı Giriniz = ");
+            kenar_2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Hipotenüs = " + (Hipotenüs = Math.Pow(((Math.Pow(kenar_1, 2)) + (Math.Pow(kenar_2, 2))), 0.5)));
+
+
+
+        }
+    }
+}
